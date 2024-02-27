cbcdf896135487aa269a6715ac38e9af3d73cc21
Marc Rietz <mwrietz@gmail.com>
2024-02-27 10:24:16 -0600

    add splash()

--- src/main.rs
+++ src/main.rs
@@ -7,69 +7,71 @@ mod tui_menu;
 fn main() {
     let mut termstat = tui_gen::TermStat::default();
 
-    tui_gen::cls();
+    tui_gen::splash_screen("Line One", "Line Two");
 
-    tui_gen::print_page_header("TITLE");
-
-    //println!("{}", tui_gen::get_prog_name());
-    termstat.line_check();
-
-    let iv: i32 = tui_inp::get_val("Enter int value: ");
-    println!("{}", iv);
-
-    let fv: f64 = tui_inp::get_val("Enter float value: ");
-    println!("{}", fv);
-
-    let iv: i32 = tui_inp::get_val_default("Enter int value: ", 3);
-    println!("{}", iv);
-
-    let fv: f64 = tui_inp::get_val_default("Enter float value: ", 43.234);
-    println!("{}", fv);
-
-    let sv: String = tui_inp::get_val("Enter string: ");
-    println!("{}", sv);
-
-    let mfrm = tui_frm::MsgFrame {
-        frame: tui_frm::Frame {
-            title: "temp",
-            title_color: Color::Blue,
-            frame_color: Color::Yellow,
-            x: 5,
-            y: 25,
-            w: 40,
-            h: 5,
-        },
-        msg: vec![
-            "line one",
-            "line two",
-            "line three",
-            "line four",
-            "line five",
-        ],
-    };
-    mfrm.frame.display();
-
-    mfrm.frame.clear();
-    mfrm.display_msg();
-    tui_gen::pause();
-
-    tui_gen::cls();
-    tui_gen::print_page_header("TITLE");
-
-    // menu
-    let menu_items = vec![
-        ("a", "Add"),
-        ("r", "Remove"),
-        ("e", "Edit"),
-        ("d", "Details"),
-        ("s", "Summary"),
-        ("m", "Menu"),
-        ("q", "Quit"),
-    ];
-
-    let val = tui_menu::menu_horiz_neo(&menu_items);
-    tui_gen::cls();
-    println!("{}", val);
+    // tui_gen::cls();
+    //
+    // tui_gen::print_page_header("TITLE");
+    //
+    // //println!("{}", tui_gen::get_prog_name());
+    // termstat.line_check();
+    //
+    // let iv: i32 = tui_inp::get_val("Enter int value: ");
+    // println!("{}", iv);
+    //
+    // let fv: f64 = tui_inp::get_val("Enter float value: ");
+    // println!("{}", fv);
+    //
+    // let iv: i32 = tui_inp::get_val_default("Enter int value: ", 3);
+    // println!("{}", iv);
+    //
+    // let fv: f64 = tui_inp::get_val_default("Enter float value: ", 43.234);
+    // println!("{}", fv);
+    //
+    // let sv: String = tui_inp::get_val("Enter string: ");
+    // println!("{}", sv);
+    //
+    // let mfrm = tui_frm::MsgFrame {
+    //     frame: tui_frm::Frame {
+    //         title: "temp",
+    //         title_color: Color::Blue,
+    //         frame_color: Color::Yellow,
+    //         x: 5,
+    //         y: 25,
+    //         w: 40,
+    //         h: 5,
+    //     },
+    //     msg: vec![
+    //         "line one",
+    //         "line two",
+    //         "line three",
+    //         "line four",
+    //         "line five",
+    //     ],
+    // };
+    // mfrm.frame.display();
+    //
+    // mfrm.frame.clear();
+    // mfrm.display_msg();
+    // tui_gen::pause();
+    //
+    // tui_gen::cls();
+    // tui_gen::print_page_header("TITLE");
+    //
+    // // menu
+    // let menu_items = vec![
+    //     ("a", "Add"),
+    //     ("r", "Remove"),
+    //     ("e", "Edit"),
+    //     ("d", "Details"),
+    //     ("s", "Summary"),
+    //     ("m", "Menu"),
+    //     ("q", "Quit"),
+    // ];
+    //
+    // let val = tui_menu::menu_horiz_neo(&menu_items);
+    // tui_gen::cls();
+    // println!("{}", val);
 
     /*
     let q = tui_inp::get_float_default("test", 32.234);
